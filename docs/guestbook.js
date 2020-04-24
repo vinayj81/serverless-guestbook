3031eadcf5992c98f42d196e57d853e664f2536a
vinayj81 <63698912+vinayj81@users.noreply.github.com>
2020-04-24 13:35:19 +0530

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://749091c0.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://2eee7c45.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
